9e3d010ba665bb0d0774e3f5a235482586fd67fd
RisingStockPrices <thswndms98@naver.com>
2020-11-21 03:32:15 +0900

    :sparkles: alignment of default humanoid and garment is done!

--- script.py
+++ script.py
@@ -19,12 +19,23 @@ class Humanoid:
         
         self.object = bpy.data.objects.get(mesh_name)
         self.armature = bpy.data.objects.get(armature_name)
+        self.matrix_world = self.object.matrix_world
         
     def get_bone(self, name):
         if name in self.armature.data.bones.keys():
             return self.armature.data.bones.get(name)
         else:
             return None
+    
+    def bone_world_co(self, name):
+        bn = self.get_bone(name)
+        if bn is not None:
+            return self.matrix_world @ bn.head_local, self.matrix_world @ bn.tail_local
+        else:
+            return None, None
+        
+    def to_world_co(self, co):
+        return self.matrix_world @ co
         
 
 default = Humanoid('default_mesh','default', 'spine01','neck','upperarm_R','upperarm_L')
@@ -97,10 +108,8 @@ def tshirt_region(humanoid, right_elbow, right_shoulder, left_elbow, left_should
     for i in range(3):
         groups[i].add(indices[i], 0.5, 'REPLACE')
         
-def tshirt_region(humanoid):
+def tshirt_region(humanoid, garment):
     group_name = ['TSHIRT_TORSO', 'TSHIRT_LEFT', 'TSHIRT_RIGHT']     
-    groups = []
-    indices = [[],[],[]]
     
     obj = humanoid.object
     
@@ -108,9 +117,6 @@ def tshirt_region(humanoid):
         if name in obj.vertex_groups.keys():
             print('vertex groups already exist for '+humanoid.name)
             return
-    
-    for nm in group_name:
-        groups.append(obj.vertex_groups.new(name = nm))
             
     global_mtx = humanoid.object.matrix_world        
     bones = humanoid.armature.data.bones
@@ -127,20 +133,26 @@ def tshirt_region(humanoid):
     left_elbow = (global_mtx @ left_arm.tail_local)[0]
     left_shoulder = (global_mtx @ left_arm.head_local)[0]
 
-    vertices = obj.data.vertices
-    for v in vertices:
-        co = obj.matrix_world @ v.co
+    for object in [obj, garment]:
+        groups = []
+        indices = [[],[],[]]
+        for nm in group_name:
+            groups.append(object.vertex_groups.new(name=nm))
         
-        if co[2] < neck_z and co[2] > spine_z:
-            if co[0] < left_elbow and co[0] >= left_shoulder:
-                indices[1].append(v.index)
-            elif co[0] < left_shoulder and co[0] >= right_shoulder:
-                indices[0].append(v.index)
-            elif co[0] < right_shoulder and co[0] >= right_elbow:
-                indices[2].append(v.index)
-    
-    for i in range(3):
-        groups[i].add(indices[i], 0.5, 'REPLACE')
+        vertices = object.data.vertices
+        for v in vertices:
+            co = object.matrix_world @ v.co
+            if co[2] < neck_z and co[2] > spine_z:
+                if co[0] < left_elbow and co[0] >= left_shoulder:
+                    indices[1].append(v.index)
+                elif co[0] < left_shoulder and co[0] >= right_shoulder:
+                    indices[0].append(v.index)
+                elif co[0] < right_shoulder and co[0] >= right_elbow:
+                    indices[2].append(v.index)
+        for i in range(3):
+            groups[i].add(indices[i], 0.5, 'REPLACE')           
+        
+   
         
 def tshirt_main_bones(humanoid, armature, right_upper_arm, left_upper_arm, neck, spine):
     right_arm = 0
@@ -165,13 +177,8 @@ def tshirt_main_bones(humanoid, armature, right_upper_arm, left_upper_arm, neck,
     
     return right_elbow, right_shoulder, left_elbow, left_shoulder, neck_z, spine_z
 
-#right_elbow, right_shoulder, left_elbow, left_shoulder, neck_z, spine_z = tshirt_main_bones(humanoid_input, armature_input, 'Upper Arm.R', 'Upper Arm.L', 'Neck', 'Spine')
 
-#right_elbow, right_shoulder, left_elbow, left_shoulder, neck_z, spine_z = tshirt_main_bones(humanoid_default, armature_default, 'upperarm_R', 'upperarm_L', 'neck', 'spine01')
-#print(right_elbow, right_shoulder, left_elbow, left_shoulder, neck_z, spine_z)
-
-#tshirt_region(humanoid_default, right_elbow, right_shoulder, left_elbow, left_shoulder, neck_z, spine_z)
-tshirt_region(default)
+tshirt_region(default, garment)
 
 # all vectors should be in world coordinates
 def arm_2_arm_transformation(src, dst, src_trans, dst_trans):
@@ -230,11 +237,9 @@ def torso_transformation(default, input):
     
     # matrix computation for each alignment step!
     
-    
     # translate to the origin first
-    trnsl = mathutils.Matrix.Translation(-def_co[neck])
-    
-    
+    trnsl = mathutils.Matrix.Translation(-def_co[neck])  
+
     factor = abs(vec_inp[right][0]/vec_def[right][0])
     scl_x = mathutils.Matrix.Scale(factor,4,[-1,0,0])
     factor =  abs(vec_inp[left][0]/vec_def[left][0])
@@ -247,12 +252,12 @@ def torso_transformation(default, input):
     # shear to match shoulder
     f1 = (vec_def[right][1]-vec_inp[right][1])/vec_def[right][0]
     f2 = (vec_inp[right][2]-vec_def[right][2])/vec_inp[right][0]
-    shear = mathutils.Matrix.Shear('XZ', 4, [0,f2])
+    shear = mathutils.Matrix.Shear('XZ', 4, [0,-abs(f2)])
     #shear2 = mathutils.Matrix.Shear('XY', 4, [0,f1])
     
     f1 = (vec_def[left][1]-vec_inp[left][1])/vec_def[left][0]
     f2 = (vec_inp[left][2]-vec_def[left][2])/vec_inp[left][0]
-    shear_ = mathutils.Matrix.Shear('XZ', 4, [0,f2])
+    shear_ = mathutils.Matrix.Shear('XZ', 4, [0,-abs(f2)])
     #shear2_ = mathutils.Matrix.Shear('XY', 4, [0,f1])
     
     
@@ -266,74 +271,6 @@ def torso_transformation(default, input):
     print(mtx_right @ def_co[spine])
     print(inp_co[spine])
     return mtx_right, mtx_left, mtx_right @ def_co[right], mtx_left @ def_co[left]
-
-
-mtx_world = default.object.matrix_world
-vertices = default.object.data.vertices
-
-vg_torso = default.object.vertex_groups.get('TSHIRT_TORSO')        
-vg_right = default.object.vertex_groups.get('TSHIRT_RIGHT')
-vg_left = default.object.vertex_groups.get('TSHIRT_LEFT')
-"""
-def_right_arm = default.get_bone((default.upperarmR))
-def_src = default.object.matrix_world @ def_right_arm.head_local
-def_dst = default.object.matrix_world @ def_right_arm.tail_local
-
-inp_right_arm = input.get_bone(input.upperarmR)
-inp_src = input.object.matrix_world @ inp_right_arm.head_local
-inp_dst = input.object.matrix_world @ inp_right_arm.tail_local
-
-mtx = arm_2_arm_transformation(def_src, def_dst, inp_src, inp_dst)
-
-vg_right = default.object.vertex_groups.get('TSHIRT_RIGHT')
-for v in vertices:
-    for group in v.groups:
-        if group.group == vg_right.index:
-            v.co = mtx_world.inverted() @ mtx @ mtx_world @ v.co
-            
-def_left_arm = default.get_bone((default.upperarmL))
-def_src = default.object.matrix_world @ def_left_arm.head_local
-def_dst = default.object.matrix_world @ def_left_arm.tail_local
-
-inp_left_arm = input.get_bone(input.upperarmL)
-inp_src = input.object.matrix_world @ inp_left_arm.head_local
-inp_dst = input.object.matrix_world @ inp_left_arm.tail_local
-
-mtx = arm_2_arm_transformation(def_src, def_dst, inp_src, inp_dst)
-
-vg_left = default.object.vertex_groups.get('TSHIRT_LEFT')
-for v in vertices:
-    for group in v.groups:
-        if group.group == vg_left.index:
-            v.co = mtx_world.inverted() @ mtx @ mtx_world @ v.co
-
-     
-
-mtx_right, mtx_left = torso_transformation(default,input)
-
-vg_torso = default.object.vertex_groups.get('TSHIRT_TORSO')
-for v in vertices:
-    for group in v.groups:
-        if group.group == vg_torso.index:
-            if v.co[0]<0:
-                v.co = mtx_world.inverted() @  mtx_right @ mtx_world @ v.co
-            else:
-                v.co = mtx_world.inverted() @ mtx_left @ mtx_world @ v.co
-    
-""" 
-
-
-mtx_right, mtx_left, right_pos, left_pos = torso_transformation(default,input)
-
-
-
-for v in vertices:
-    for group in v.groups:
-        if group.group in [vg_left.index, vg_right.index, vg_torso.index]:
-            if v.co[0]<0:
-                v.co = mtx_world.inverted() @  mtx_right @ mtx_world @ v.co
-            else:
-                v.co = mtx_world.inverted() @ mtx_left @ mtx_world @ v.co
 
 def arm_transformation(common_origin, def_vec, inp_vec):
     
@@ -352,65 +289,117 @@ def arm_transformation(common_origin, def_vec, inp_vec):
     trnsl_final = mathutils.Matrix.Translation(common_origin)
     
     return trnsl_final @ shear @ scl_x @ trnsl
+
+
+def duplicate_object(obj):
+    print(bpy.context.selected_objects)
+    for obj in bpy.context.selected_objects:
+        obj.select_set(state=False)
     
+    obj.select_set(state=True)
+    bpy.ops.object.duplicate()
+    
+    dup_obj = bpy.context.selected_objects[0]
+    dup_obj.select_set(state=False)
+    return dup_obj
 
-def_right_arm = default.get_bone((default.upperarmR))
-def_src = default.object.matrix_world @ def_right_arm.head_local
-def_dst = default.object.matrix_world @ def_right_arm.tail_local
+# create a replica of default humanoid to manipulate if not already there
+name = default.name +'.001'
+if name not in bpy.data.objects.keys():
+    default_aligned = duplicate_object(default.object)
+    garment_aligned = duplicate_object(garment)
+    
+    mtx_world = default_aligned.matrix_world
+    vertices = default_aligned.data.vertices
+    
+    vg_torso = default_aligned.vertex_groups.get('TSHIRT_TORSO')        
+    vg_right = default_aligned.vertex_groups.get('TSHIRT_RIGHT')
+    vg_left = default_aligned.vertex_groups.get('TSHIRT_LEFT')
+    vgs = [vg_torso.index, vg_right.index, vg_left.index]
 
-inp_right_arm = input.get_bone(input.upperarmR)
-inp_src = input.object.matrix_world @ inp_right_arm.head_local
-inp_dst = input.object.matrix_world @ inp_right_arm.tail_local
+    # leave only the region of interest and delete the rest
+    default_aligned.select_set(state=True)
+    bpy.ops.object.mode_set(mode = 'OBJECT')
+    for v in vertices:
+        check = False
+        for group in v.groups:
+            if group.group in vgs:
+                check = True
+                break
+        if not check:
+            v.select = True
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.delete(type = 'VERT')
+    bpy.ops.object.mode_set(mode = 'OBJECT')
 
-def_vec = def_dst-def_src
-inp_vec = inp_dst - inp_src
 
-mtx = arm_transformation(right_pos,def_vec,inp_vec)
-#mtx = arm_2_arm_transformation(def_src, def_dst, inp_src, inp_dst)
+    # transform to fit the torso
+    mtx_right, mtx_left, right_pos, left_pos = torso_transformation(default,input)
+    
+    for v in vertices:
+        if v.co[0]<0:
+            v.co = mtx_world.inverted() @  mtx_right @ mtx_world @ v.co
+        else:
+            v.co = mtx_world.inverted() @ mtx_left @ mtx_world @ v.co
+        
 
-vg_right = default.object.vertex_groups.get('TSHIRT_RIGHT')
-for v in vertices:
-    for group in v.groups:
-        if group.group == vg_right.index:
-            v.co = mtx_world.inverted() @ mtx @ mtx_world @ v.co
+    # transform to fit both arms
+    def_src,def_dst = default.bone_world_co(default.upperarmR)
+    inp_src,inp_dst = input.bone_world_co(input.upperarmR)
+    mtxR = arm_transformation(right_pos,def_dst-def_src, inp_dst-inp_src)
+    
+    def_src,def_dst = default.bone_world_co(default.upperarmL)
+    inp_src,inp_dst = input.bone_world_co(input.upperarmL)  
+    mtxL = arm_transformation(left_pos,def_dst-def_src, inp_dst-inp_src)
+    
+    for v in vertices:
+        for group in v.groups:
+            if group.group == vg_right.index:
+                v.co = mtx_world.inverted() @ mtxR @ mtx_world @ v.co
+            elif group.group == vg_left.index:
+                v.co = mtx_world.inverted() @ mtxL @ mtx_world @ v.co
+                
+else:
+    default_aligned = bpy.data.objects.get(name)
+    garment_aligned = bpy.data.objects.get(GARMENT+'.001')            
             
-def_left_arm = default.get_bone((default.upperarmL))
-def_src = default.object.matrix_world @ def_left_arm.head_local
-def_dst = default.object.matrix_world @ def_left_arm.tail_local
-
-inp_left_arm = input.get_bone(input.upperarmL)
-inp_src = input.object.matrix_world @ inp_left_arm.head_local
-inp_dst = input.object.matrix_world @ inp_left_arm.tail_local
-
-mtx = arm_transformation(left_pos,def_vec,inp_vec)
-#mtx = arm_2_arm_transformation(def_src, def_dst, inp_src, inp_dst)
-
-vg_left = default.object.vertex_groups.get('TSHIRT_LEFT')
-for v in vertices:
-    for group in v.groups:
-        if group.group == vg_left.index:
-            v.co = mtx_world.inverted() @ mtx @ mtx_world @ v.co
-            
-            
-def ray_cast(humanoid, target):
-    vgs = humanoid.object.vertex_groups
-    vg = [vgs.get('TSHIRT_LEFT').index, vgs.get('TSHIRT_RIGHT').index, vgs.get('TSHIRT_TORSO').index]
-    vertices = huamnoid.object.data.vertices
+def ray_cast(ROI, target):
+    vertices = ROI.data.vertices
     
     results = []
     for v in vertices:
-        for grp in v.groups:
-            if grp.group in vgs:
-                # local coordinates of the rays onto model coordinate system
-                localC = target.matrix_world.inverted() @ v.co
-                localN = target.matrix_world.inverted() @ v.normal
-                
-                # apply ray casting, store the result
-                collision, location, n, i = target.ray_cast([localC[0], localC[1], localC[2]],[localN[0],localN[1],localN[2]])
-                
-                location_global = target.matrix_world @ location
-                results.append({'index':v.index,'collision':collision,'location':location_global})
-                
+        # local coordinates of the rays onto model coordinate system
+        localC = target.matrix_world.inverted() @ v.co
+        localN = target.matrix_world.inverted() @ v.normal
+        
+        # apply ray casting, store the result
+        collision, location, n, i = target.ray_cast([localC[0], localC[1], localC[2]],[localN[0],localN[1],localN[2]])
+        
+        location_global = target.matrix_world @ location
     
+        results.append({'index':v.index,'collision':collision,'location':location_global})
+        
     return results
+
+
+def_inp_ray_cast = ray_cast(default_aligned, input.object)
+def_gar_ray_cast = ray_cast(default_aligned, garment_aligned)
+
+vertices =garment_aligned.data.vertices
+mtx_world = garment_aligned.matrix_world
+vg_right = garment_aligned.vertex_groups.get('TSHIRT_RIGHT')
+vg_left = garment_aligned.vertex_groups.get('TSHIRT_LEFT')
+
+for v in vertices:
+    if v.co[0]<0:
+        v.co = mtx_world.inverted() @  mtx_right @ mtx_world @ v.co
+    else:
+        v.co = mtx_world.inverted() @ mtx_left @ mtx_world @ v.co
+
+for v in vertices:
+        for group in v.groups:
+            if group.group == vg_right.index:
+                v.co = mtx_world.inverted() @ mtxR @ mtx_world @ v.co
+            elif group.group == vg_left.index:
+                v.co = mtx_world.inverted() @ mtxL @ mtx_world @ v.co
 
